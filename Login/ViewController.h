5db6788bc6ba02c8f4b10b8a47ebe1b6928e7c92
raghunv <nvraghu007@gmail.com>
2013-09-22 08:54:34 +0530

    Mse_login_app

--- Login/ViewController.h
+++ Login/ViewController.h
@@ -9,5 +9,8 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *appleID;
+@property (weak, nonatomic) IBOutlet UITextField *password;
+- (IBAction)loginButton:(id)sender;
 
 @end
--- Login/ViewController.m
+++ Login/ViewController.m
@@ -7,12 +7,15 @@
 //
 
 #import "ViewController.h"
+#import "HomeScreen.h"
 
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+@synthesize appleID;
+@synthesize password;
 
 - (void)viewDidLoad
 {
@@ -26,4 +29,35 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)loginButton:(id)sender {
+    
+    NSString *username = appleID.text;
+    NSString *pass = password.text;
+    
+    if ([username isEqualToString:pass]) {
+        
+        HomeScreen *hs = [[HomeScreen alloc]init];
+        [hs setTemp:username];
+        [self presentViewController:hs animated:YES completion:nil];
+        
+    }
+    else{
+        UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"Login Failed" message:@"invalid details" delegate:nil cancelButtonTitle:nil otherButtonTitles:@"OK", nil];
+        UIImageView *imageView=[[UIImageView alloc] initWithFrame:CGRectMake(90,-120,120,120 )];
+        NSString *path=[[NSString alloc] initWithString:[[[NSBundle mainBundle] resourcePath]stringByAppendingPathComponent:@"images.jpg"]];
+        
+        
+        
+        UIImage *bkgimg =[[UIImage alloc] initWithContentsOfFile:path];
+        [imageView setImage:bkgimg];
+       // [bkgimg release];
+        //[path release];
+        [alert addSubview:imageView];
+        //[imageView release];
+        
+        
+        [alert show];
+        //[alert release];
+    }
+}
 @end
